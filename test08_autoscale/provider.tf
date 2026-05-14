9775200979498728e6329825c66f78c6ae07d5f8
yimjongwon <awosung00@naver.com>
2026-05-14 12:04:18 +0900

    autoscale예제 추가함

---
+++ test08_autoscale/provider.tf
@@ -0,0 +1,14 @@
+# 테라폼 및 프로바이더 설정
+terraform {
+  required_version = "~> 1.14.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
